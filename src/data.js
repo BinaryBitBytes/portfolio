7edf0b6c1cfff106e6bd8396bec1685ec416466a
BinaryBitBytes <117mwc117@gmail.com>
2023-06-11 18:58:30 -0400

    created a data.js witin the src folder to utilize JSON to store project data information that will be showcased within the project

---
+++ src/data.js
@@ -0,0 +1,24 @@
+// Creating an array of Projects in JSON
+export const projects = [
+    {
+        title: 'ReadMe_Gen' ,
+        subtitle: 'JQuery, Javascript, & NodeJS' ,
+        description: 'A readme generator template that will make generating a professional looking readme simply and easily by following prompts in node.js.',
+        image: '../public/ProjectImages/ReadMeGen.gif' ,
+        link: 'https://github.com/BinaryBitBytes/ReadMe_Gen',
+    },
+    {
+        title: 'Password-Generator',
+        subtitle: 'HTML, CSS, & Javascript' ,
+        description: 'This is a basic password generator. Enjoy!',
+        image: '../public/ProjectImages/passwordGenImg.png',
+        link: 'https://binarybitbytes.github.io/Password-Generator/',
+    },
+    {
+        title: 'Sunshine- Activity Suggester',
+        subtitle: 'Open Weather API, TrueWay Places API, Local Storage, Foundation.JS, Javascript, & HTML',
+        description:'The Sunshine Application allows a social enthusiasts to pick a desired destination city in order to find appropriate activities based on ambient conditions at that destination and user intent. It also takes local coordinates in order to provide weather conditions of user current location.' ,
+        image: '../public/ProjectImages/Sunshine.gif',
+        link: 'https://binarybitbytes.github.io/Sunshine/',
+    },
+]
