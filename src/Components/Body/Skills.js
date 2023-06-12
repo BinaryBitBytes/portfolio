90a158f9142fd13f0942e84508b462b804f5f468
BinaryBitBytes <117mwc117@gmail.com>
2023-06-12 12:28:41 -0400

    completed skills.js

--- src/Components/Body/Skills.js
+++ src/Components/Body/Skills.js
@@ -16,18 +16,18 @@ export default function Skills() {
                     </p>
                 </div>
                 <div className='flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2'>
-                    {skills.map(skill) => (
+                    {skills.map((skill) => (
                         <div key={skill} className='p-2 sm:w-1/2 w-full'>
                             <div className='bg-gray-800 rounded flex p-4 h-full items-center'>
                                 <BadgeCheckIcon className='text-green-400 w-6 h-6 flex-shrink-0 mr-4' />
                                 <span className='title-font font-medium text-white'>
                                     {skill}
-                                    </span>
-                                    </div>
-                                    </div>
-                    )}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
