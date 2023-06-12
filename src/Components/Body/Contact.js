5c4ee665b01efae0dda91c778465f7a806901704
BinaryBitBytes <117mwc117@gmail.com>
2023-06-12 13:05:25 -0400

    updated the contact.js, needs completion

--- src/Components/Body/Contact.js
+++ src/Components/Body/Contact.js
@@ -1,4 +1,8 @@
 import React from "react";
 export default function Contact() {
-
+    <section id='contact' className='relative'>
+        <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
+            
+        </div>
+    </section>
 }
