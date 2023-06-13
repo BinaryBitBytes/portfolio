da5256637295ba666d16264367b74e6327199f77
BinaryBitBytes <117mwc117@gmail.com>
2023-06-12 23:39:05 -0400

    updated and completed contact.js & navbar.js

--- src/Components/Body/Contact.js
+++ src/Components/Body/Contact.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 export default function Contact() {
+    const { name, setName } = React.useState("")
+    const { email, setEmail } = React.useState("")
+    const { message, setMessage } = React.useState("")
+    
+    function encode(data) {
+        return Object.keys(data)
+        .map(
+            (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        )
+        .join('&')
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({ 'form-name': 'contact', name, email, message }),
+        })
+        .then(() => alert('Message was sent!'))
+        .catch((error) => alert(error))
+    }
+
     return(
         <section id='contact' className='relative'>
             <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
@@ -14,7 +37,7 @@ export default function Contact() {
                     marginHeight={0}
                     marginWidth={0}
                     style={{ filter: 'opacity(0.6' }}
-                    src='https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8'
+                    src='https://www.google.com/maps/embed/v1/place?q=Lillian+Dr,+Newnan,+GA+30265,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8'
                     />
                     <div className='bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md'>
                         <div className='lg:w-1/2 px-6'>
@@ -43,6 +66,7 @@ export default function Contact() {
                 <form
                 netlify
                 name='contact'
+                onSubmit={handleSubmit}
                 className='lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0'>
                 <h2 className='text-white sm:text-4x1 text-3x1 mb-1 font-medium title-font'>
                     Hire Me - Looking For Work
@@ -60,6 +84,7 @@ export default function Contact() {
                     id='name'
                     name='name'
                     className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+                    onChange={(e) => setName(e.target.value)}
                     />
                 </div>
                 <div className='relative mb-4'>
@@ -71,6 +96,7 @@ export default function Contact() {
                     id='email'
                     name='email'
                     className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+                    onChange={(e) => setEmail(e.target.value)}
                     />
                     </div>
                     <div className='relative mb-4'>
@@ -83,6 +109,7 @@ export default function Contact() {
                         id='message'
                         name='message'
                         className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
+                        onChange={(e) => setMessage(e.target.value)}
                         />
                     </div>
                     <button
