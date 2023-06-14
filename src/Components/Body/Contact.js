3c9576af14d4dca1ac4b0e9a760df69bb17edb86
BinaryBitBytes <117mwc117@gmail.com>
2023-06-13 21:48:06 -0400

    finally got the react app to run, just need to refine the tailwindCSS styling

--- src/Components/Body/Contact.js
+++ src/Components/Body/Contact.js
@@ -53,7 +53,7 @@ export default function Contact() {
                             <h2 className='title-font font-semibold text-white tracking-widest text-xs'>
                                 EMAIL
                             </h2>
-                            <a className='text-indigo-400 leading-relaxed'>
+                            <a className='text-indigo-400 leading-relaxed' href='email-address'>
                                 117MWC117@GMAIL.COM
                             </a>
                             <h2 className='title-font font-semibold text-white tracking-widest text-xs mt-4'>
--- src/Components/Body/Projects/Projects.js
+++ src/Components/Body/Projects/Projects.js
@@ -1,6 +1,6 @@
 import { CodeIcon } from '@heroicons/react/solid'
 import React from 'react'
-import { projects } from '../data'
+import { projects } from '../../../Data/data.js'
 
 export default function Projects() {
     return (
