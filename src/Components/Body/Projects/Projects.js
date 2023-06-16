3794ebeb89f5ba79acf6d371388d9e0fa56e0261
BinaryBitBytes <117mwc117@gmail.com>
2023-06-16 09:54:04 -0400

    resolved the styling of the cards in projects

--- src/Components/Body/Projects/Projects.js
+++ src/Components/Body/Projects/Projects.js
@@ -18,6 +18,7 @@ export default function Projects() {
                             <ol>ReadMe Generator- JQuery / NodeJS</ol>
                             <ol>Password Generator- HTML / Javascript</ol>
                             <ol>Sunshine Places Recommendation App- Javascript / HTML / Bootstrap / API</ol>
+                            <br></br>
                         </li>
                     </p>
                 </div>
@@ -26,14 +27,14 @@ export default function Projects() {
                         <a
                         href={project.link}
                         key={project.key}
-                        className='sm:w-1/2 w-100 p-4 flex flex-wrap -m-4 border-3 border-indigo-600 rounded-full'>
-                        <div className='flex relative '>
+                        className='sm:w-1/2 w-100 p-6 flex flex-wrap -m-4 border-3 border-indigo-600 rounded-full'>
+                        <div className='flex-auto relative '>
                             <img
                             alt='gallery'
                             className='absolute inset-0 h-full w-full object-cover object-center'
                             src={project.image}
                             />
-                            <div className='px-10 py-10 relative z-10 w-full border-4 border-gray-800 bg0gray-900 opacity-0 hover:opacity-100'>
+                            <div className='px-10 py-10 relative z-10 w-full h-full border-4 border-indigo-800 bg-gray-900 opacity-0 hover:opacity-100'>
                                 <h2 className='tracking-widest text-sm title-font font-medium text-green-400 mb-1'>
                                     {project.subtitle}
                                 </h2>
