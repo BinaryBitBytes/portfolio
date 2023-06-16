6b798b41b3629b172293ad910080e236e309f212
BinaryBitBytes <117mwc117@gmail.com>
2023-06-16 14:29:28 -0400

    updating changes with declerations

--- src/Components/Header/Name.js
+++ src/Components/Header/Name.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactRevealText from 'react-reveal-text/lib/ReactRevealText.js'
+import ReactRevealText from 'react-reveal-text'
 
 export default function Name() {
     return (
-        <ReactRevealText
-            text= 'Miles Colòn'
-            className= 'my-name'
-            style={{color: "tomato", fontSize: "24px", lineHeight: "36px"}}
-        />
+        <div>
+            <ReactRevealText
+                text= 'Miles Colòn'
+                className= 'my-name'
+                style={{color: "tomato", fontSize: "24px", lineHeight: "36px"}}
+            />
+        </div>
     )
 }
