8e46e4a7bd9bec9a0c4935978685d1ac4464b22e
BinaryBitBytes <117mwc117@gmail.com>
2023-06-16 15:43:32 -0400

    was able to get the name text effect to function

--- src/Components/Header/Name.js
+++ src/Components/Header/Name.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactRevealText from 'react-reveal-text'
 
-export default function Name() {
-    return (
-        <div>
-            <ReactRevealText
-                text= 'Miles Colòn'
-                className= 'my-name'
-                style={{color: "tomato", fontSize: "24px", lineHeight: "36px"}}
-            />
+class Name extends React.Component {
+
+    constructor() {
+      super();
+      this.state = { show: false };
+    }
+  
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({ show: true });
+      }, 2200);
+    }
+  
+    render() {
+      return (
+        <div style={{ 
+            fontFamily: 'fantasy',
+            border: '5px double indigo', padding: '12px',
+            textAlign: 'center', color: "tomato", 
+            fontSize: "32px", lineHeight: "36px"}}>
+          <h1>
+            <ReactRevealText 
+            show={this.state.show}>
+                Miles Colòn 
+            </ReactRevealText>
+          </h1>
         </div>
-    )
-}
+      );
+    }
+  }
+  export default Name
--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -3,33 +3,33 @@ import { ArrowRightIcon } from '@heroicons/react/solid'
 import Name from "./name";
 
 export default function Navbar() {
-    return (
-        <header className="bg-gray-800 md:sticky top-0 z-10">
-          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a className="title-font font-medium text-white mb-4 md:mb-0"
-              href='Navigation Bar'>
-              <a href="#about" className="ml-3 text-xl">
-                <Name />
-              </a>
-            </a>
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-              <a href="#projects" className="mr-5 text-red-300 hover:text-white">
-                Past Work
-              </a>
-              <a href="#skills" className="mr-5 text-white hover:text-blue-300">
-                My Skills In The Industry
-              </a>
-              <a href="#testimonials" className="mr-5 text-blue-300 hover:text-red-300">
-                Client Testimonials
-              </a>
-            </nav>
-            <a
-              href="#contact"
-              className="inline-flex items-center bg-green-900 hover:text-purple-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-500 rounded text-base mt-4 md:mt-0">
-              Hire Me & Looking To Work
-              <ArrowRightIcon className="w-4 h-4 ml-1" />
-            </a>
-          </div>
-        </header>
-      );
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0" href='Navigation Bar'>
+          <a href="#about" className="ml-3 text-xl flex">
+            <Name />
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 text-red-300 hover:text-white">
+            Past Work
+          </a>
+          <a href="#skills" className="mr-5 text-white hover:text-blue-300">
+            My Skills In The Industry
+          </a>
+          <a href="#testimonials" className="mr-5 text-blue-300 hover:text-red-300">
+            Client Testimonials
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-green-900 hover:text-purple-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-500 rounded text-base mt-4 md:mt-0"
+        >
+          Hire Me & Looking To Work
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a>
+      </div>
+    </header>
+  );
 }
