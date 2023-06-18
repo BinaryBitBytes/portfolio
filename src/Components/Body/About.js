be6a18e4e1b654f4e984bf0d864c6a1f19b928ef
BinaryBitBytes <117mwc117@gmail.com>
2023-06-18 13:06:20 -0400

    started refining the About.js, on line 7 still trying to implement a background image

--- src/Components/Body/About.js
+++ src/Components/Body/About.js
@@ -2,9 +2,13 @@ import React from "react";
 export default function About() {
     return(
     <section id='about'>
-        <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
+        <div 
+        className='container mx-auto flex px-3 py-3 md:flex-row flex-col items-center' 
+        class="bg-center bg-cover bg-[url('https://github.com/BinaryBitBytes/portfolio/blob/ca40fbe73c6fc70c8ed4688228a68c5f1f3f5260/public/coding.png')]"
+        alt='background-AboutMe'>
+            </div>
             <div className='lg:flex-grow md:q-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
-                <h1 className='title-font sm:text-4x1 text 3x1 md-4 font-medium text-white'>
+                <h1 className='title-font sm:text-4x1 text 3x1 md-4 font-medium text-neon'>
                     Welcome to my portfolio, my name is Miles Colon.
                     <br className='hidden lg:inline-block' /> I enjoy programming, especially building web applications and unit tests.
                 </h1>
@@ -16,27 +20,22 @@ export default function About() {
                 <div className="flex justify-center">
                     <a
                     href='#contact'
-                    className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'>
-                        Looking 4 Work
-                        Email: 117mwc117@gmail.com
-                        Phone: 678-675-6882
-                        LinkedIn.com/BinaryBitBytes
+                    className='inline-flex text-white bg-green-500 border-3 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'>
+                        <p>
+                            <strong>
+                            Looking 4 Work<br />
+                            Email:117mwc117@gmail.com<br />
+                            Phone: 678-675-6882<br />
+                            LinkedIn.com/BinaryBitBytes
+                            </strong>
+                        </p>
                     </a>
                     <a
                     href='#projects'
-                    className='ml-4 inline-flex text-gray-400-bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'>
+                    className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'>
                         Checkout my hard work & projects
                     </a>
                 </div>
-            </div>
-            <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
-                <img
-                className='object-cover object-center rounded'
-                alt='hero'
-                src='../../../public/coding.png'
-                >
-                </img>
-            </div>
         </div>
     </section>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       backgroundImage: {
         'bg-pattern': "url('https://images.newscientist.com/wp-content/uploads/2021/09/01154236/PRI_197379901.jpg')",
         'footer-texture': "url('/img/footer-texture.png')",
+        'about-img': "url('https://github.com/BinaryBitBytes/portfolio/blob/ca40fbe73c6fc70c8ed4688228a68c5f1f3f5260/public/coding.png')"
       }
     },
   },
