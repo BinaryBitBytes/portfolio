320a53fe89f920b76a6fa969775b73c79aeb0917
BinaryBitBytes <117mwc117@gmail.com>
2023-06-19 18:29:41 -0400

    needs to be more responsive but card is edited

--- src/Components/Body/Testimonials.js
+++ src/Components/Body/Testimonials.js
@@ -9,7 +9,7 @@ export default function Testimonials() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
             Client Testimonials
           </h1>
-          <div className="flex flex-wrap m-4">
+          <div className=" container px-5 py-10 mx-auto flex flex-wrap m-4">
             {testimonials.map((testimonial) => (
               <div className="p-4 md:w-1/2 w-full">
                 <div className="h-full bg-rose-900 bg-opacity-40 p-8 rounded">
