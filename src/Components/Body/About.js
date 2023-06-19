9f2d8fac3c241f78b4c8f51693b54d5ce5d9bf83
BinaryBitBytes <117mwc117@gmail.com>
2023-06-19 12:53:43 -0400

    refactored the about.js by adding more divs to modularize the styling, needs to be styled

--- src/Components/Body/About.js
+++ src/Components/Body/About.js
@@ -3,26 +3,43 @@ export default function About() {
     return(
     <section id='about'>
         <div 
-        className='container mx-auto flex px-3 py-3 md:flex-row flex-col items-center ' 
+        className='container mx-auto flex px-12 py-12 md:flex-row flex-col items-center ' 
         class="bg-center bg-cover bg-[url('https://cosmosmagazine.com/wp-content/uploads/2020/02/190613_atoms_full-1440x813.jpg')]"
         alt='background-AboutMe'>
             {/* <div className='center-content flex items-center lg:flex-grow md:q-1/2 lg:pr-44 md:pr-16 flex flex-col md:items-start md:text-center mb-16 md:mb-0 items-center text-center'> */}
             <div className='center-content items-center'>
-                <div>
-                    <h1 className='flex py-12 title-font sm:text-4x1 text 3x1 md-4 font-medium text-neon' class='hover:bg-sky-800/[.5] hover:uppercase font-extrabold text-emerald-700 text-center self-center place-content-center'>
-                        Welcome to my portfolio, my name is Miles Colon.
-                        <br className='hidden lg:inline-block' /> I enjoy programming, especially building web applications and unit tests.
-                    </h1>
+                <div className=" box-border container rounded-full border-4 box-border h-12 w-55 border-indigo-800 mx-auto py-0 mb-16">
+                    <div className='box-content flex py-4 title-font sm:text-4x1 text 3x1 md-1 font-medium' class='hover:bg-sky-800/[.5] hover:uppercase font-extrabold text-emerald-700 text-center self-center place-content-center'>
+                        <h1>
+                            Welcome to my portfolio, my name is Miles Colon. I enjoy programming, especially building web applications and unit tests.
+                        </h1>
+                    </div>
                 </div>
-                <div className="container mx-auto px-48 py-7 text-center absolute inset-x-0 bottom-80 h-16">
-                    <p className='rounded-full border-4 border-indigo-800 mb-7 leading-relaxed bg-emerald-800 font-large md-8 text-rose-300'>
-                    <aside><strong className="text-5xl absolute inset-y-7 left-48 w-28">🐧</strong></aside>
-                    I specialze in full stack web development. While I am skilled in both
-                        the front end & back end of the stack,
-                        <br />I really enjoy the problem solving
-                        and meaningfulness of backend development.
-                        <aside><strong className="text-5xl absolute inset-y-7 right-48 w-28">🐧</strong></aside>
-                    </p>
+                <div className="container mx-auto px-auto py-7 text-center absolute inset-x-0 bottom-80 h-16">
+                    <div className='rounded-full border-4 border-indigo-800 leading-relaxed bg-emerald-800 font-large md-8 text-rose-300'>
+                        <p>
+                            <div className="text-5xl absolute inset-y-7 left-48 w-28">
+                                <aside>
+                                    <strong>
+                                        🐧
+                                    </strong>
+                                </aside>
+                            </div>
+                            <div>
+                                I specialze in full stack web development. While I am skilled in both
+                                the front end & back end of the stack,
+                                <br />I really enjoy the problem solving
+                                and meaningfulness of backend development.
+                            </div>
+                            <div className="text-5xl absolute inset-y-7 right-48 w-28">
+                                <aside>
+                                    <strong>
+                                        🐧
+                                    </strong>
+                                </aside>
+                            </div>
+                        </p>
+                    </div>
                 </div>
                 <br></br>
                 <div class='place-content-end h-72 content-between-20 '>
