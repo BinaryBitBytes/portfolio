fc6189ac7271abf095b8e7e939707b6f34003f6e
BinaryBitBytes <117mwc117@gmail.com>
2023-09-19 08:08:07 -0500

    created a live page production build

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -11,8 +16,3 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
