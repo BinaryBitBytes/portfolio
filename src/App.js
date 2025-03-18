0a56fa09eaf67ba1ae13dbbeb031532bf8e5b387
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2025-03-18 12:38:55 -0400

    converted from vanilla js to jsx react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import About from './Components/Body/About.js';
-import Contact from './Components/Body/Contact.js';
-import Navbar from './Components/Header/Navbar.js';
-import Projects from './Components/Body/Projects/Projects.js';
-import Skills from './Components/Body/Skills.js';
-import Testimonials from './Components/Body/Testimonials.js';
-import '../src/input.css';
+import React from "react";
+import About from "./Components/Body/About.jsx";
+import Contact from "./Components/Body/Contact.jsx";
+import Navbar from "./Components/Header/Navbar.jsx";
+import Projects from "./Components/Body/Projects/Projects.jsx";
+import Skills from "./Components/Body/Skills.jsx";
+import Testimonials from "./Components/Body/Testimonials.jsx";
+import "../src/input.css";
 // import './App.css';
 
 function App() {
   return (
-    <main >
-      <Navbar className='text-gray-400 bg-green-900 body-font' />
+    <main>
+      <Navbar className="text-gray-400 bg-green-900 body-font" />
       <About />
       <Projects />
       <Skills />
---
+++ src/Components/Header/Name.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactRevealText from "react-reveal-text/lib/ReactRevealText.js";
+
+class Name extends React.Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 2200);
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          fontFamily: "fantasy",
+          border: "5px double indigo",
+          padding: "38 px",
+          textAlign: "center",
+          color: "tomato",
+          fontSize: "32px",
+          lineHeight: "36px",
+        }}
+      >
+        <h1>
+          <ReactRevealText show={this.state.show}>Miles Colòn</ReactRevealText>
+        </h1>
+      </div>
+    );
+  }
+}
+export default Name;
