44166abac839b4ce8ccdade2335d3e3292b902a6
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2025-03-18 10:38:49 -0400

    updated gitignore & refined a import on a typescript dependancy related to reactRevealText

--- src/Components/Header/Name.js
+++ src/Components/Header/Name.js
@@ -1,34 +1,36 @@
 import React from "react";
-import ReactRevealText from 'react-reveal-text'
+import ReactRevealText from "../../../node_modules/react-reveal-text/lib/ReactRevealText.js";
 
 class Name extends React.Component {
-
-    constructor() {
-      super();
-      this.state = { show: false };
-    }
-  
-    componentDidMount() {
-      setTimeout(() => {
-        this.setState({ show: true });
-      }, 2200);
-    }
-  
-    render() {
-      return (
-        <div style={{ 
-            fontFamily: 'fantasy',
-            border: '5px double indigo', padding: '38 px',
-            textAlign: 'center', color: "tomato", 
-            fontSize: "32px", lineHeight: "36px"}}>
-          <h1>
-            <ReactRevealText 
-            show={this.state.show}>
-                Miles Colòn 
-            </ReactRevealText>
-          </h1>
-        </div>
-      );
-    }
+  constructor() {
+    super();
+    this.state = { show: false };
   }
-  export default Name
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 2200);
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          fontFamily: "fantasy",
+          border: "5px double indigo",
+          padding: "38 px",
+          textAlign: "center",
+          color: "tomato",
+          fontSize: "32px",
+          lineHeight: "36px",
+        }}
+      >
+        <h1>
+          <ReactRevealText show={this.state.show}>Miles Colòn</ReactRevealText>
+        </h1>
+      </div>
+    );
+  }
+}
+export default Name;
