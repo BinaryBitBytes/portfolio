31a513d166593e0b1c78db510f7a75c030738299
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2025-04-06 02:47:57 -0400

    production 2.0 please

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Serve the React application for all requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
